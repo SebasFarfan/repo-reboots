80a589c19cbe2aab5b1afc64239429620247c5af
sebas <sebas.654@gmail.com>
2021-05-11 18:32:39 -0300

    agregado CompraServiceImp y modificado ICompraSer

---
+++ src/main/java/ar/edu/unju/fi/tp4/service/imp/CompraServiceImp.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.tp4.service.imp;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.tp4.service.Compra;
+import ar.edu.unju.fi.tp4.service.ICompraService;
+
+@Service("compraUtilService")
+public class CompraServiceImp implements ICompraService {
+
+    private List<Compra> compras;
+	private static final Log LOGGER = LogFactory.getLog(ClienteServiceImp.class);
+
+    @Override
+    public void agregarCompra(Compra compra) {
+        compras.add(compra);
+		LOGGER.info("METHOD: agregarCompra(compra) - se agregó una compra al listado -->"
+                + compras.get(compras.size()-1));
+        
+    }
+
+    @Override
+    public List<Compra> getCompra() {
+        String cantidad = "";
+		if(this.compras == null) {
+			cantidad = "Null";
+		}else {
+			cantidad = String.valueOf(compras.size());
+		}		
+		LOGGER.info("METHOD: getCompra() - muestra el listado de compras -->"
+                + cantidad + " clientes");
+		return this.compras;
+        
+    }
+
+    
+}
--- src/main/java/ar/edu/unju/fi/tp4/service/imp/ICompraService.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.unju.fi.tp4.service.imp;
-
-public interface ICompraService {
-    public void agregarCompra(Compra compra);
-    public void getCompra();
-}
