a3fdcee77dc6b136ac039c7351c77588abfa9f7b
sebas <sebas.654@gmail.com>
2021-05-11 18:33:16 -0300

    modificado ICompraService

---
+++ src/main/java/ar/edu/unju/fi/tp4/service/ICompraService.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.tp4.service;
+
+import java.util.List;
+
+public interface ICompraService {
+    public void agregarCompra(Compra compra);
+    public List<Compra> getCompra();
+}
